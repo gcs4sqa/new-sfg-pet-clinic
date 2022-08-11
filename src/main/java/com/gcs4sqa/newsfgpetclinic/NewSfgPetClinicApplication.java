33538606f1729e8b6327a619d117aa5aa1c9395b
cliff.douglas@digital.homeoffice.gov.uk <!apple01apple01>
2022-08-11 14:26:47 +0100

    added sprint initializer files

---
+++ src/main/java/com/gcs4sqa/newsfgpetclinic/NewSfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.gcs4sqa.newsfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSfgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcs4sqa/newsfgpetclinic/NewSfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcs4sqa.newsfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
